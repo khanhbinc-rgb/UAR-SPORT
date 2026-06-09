cb7b640c0b8c421b8dcb605c1f3748d80a1da036
khanh <khanh.binc@gmail.com>
2026-06-09 14:15:15 +0700

    fix vietqr server

--- backend/server.js
+++ backend/server.js
@@ -44,8 +44,9 @@ app.post("/create-vietqr", (req, res) => {
   }
 });
 
+// ✅ BẮT BUỘC PHẢI CÓ CÁI NÀY (Render cần)
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("VietQR Server Running on port " + PORT);
+  console.log(`VietQR Server running on port ${PORT}`);
 });
