e06a80ac9f1f061e55ca766978fcd4b50649b27f
khanh <khanh.binc@gmail.com>
2026-06-10 23:25:08 +0700

    fix express route

--- backend/server.js
+++ backend/server.js
@@ -88,7 +88,7 @@ app.post("/save-order", async (req, res) => {
 ========================================= */
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
